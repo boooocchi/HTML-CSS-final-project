cf3ca3f818033982e1eea62b5205818dce023b38
boooocchi@gmail.com <boooocchi@gmail.com>
2023-01-19 19:01:54 -0800

    fianl

--- FinalProject/script.js
+++
@@ -1,6 +0,0 @@
-document.querySelector(".burger").addEventListener("click", function (e) {
-  document.querySelector(".hamburger-menu").classList.toggle("display-on");
-});
-document.querySelector(".burger").addEventListener("click", function (e) {
-  e.preventDefault();
-});
